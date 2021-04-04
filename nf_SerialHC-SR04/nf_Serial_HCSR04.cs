c79ca9a756b082ac1d5da77ef15fdbc07f09589d
up_stream <up_stream@hotmail.com>
2021-04-04 05:37:03 -0300

    Added suport for STM32F769IDiscovery board

--- nf_SerialHC-SR04/nf_Serial_HCSR04.cs
+++ nf_SerialHC-SR04/nf_Serial_HCSR04.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿// NOTE: when working with ESP32 this define needs to be uncommented
+#define BUIID_FOR_ESP32
+
+using System;
 using System.Threading;
 using System.Diagnostics;
 using Windows.Storage.Streams;
 using Windows.Devices.SerialCommunication;
+using nanoFramework.Runtime.Events; /// New. Needs Packages
 using nanoFramework.Hardware.Esp32;
 using Driver.nf_SerialHC_SR04.Constants;
 
@@ -10,11 +14,13 @@ using Driver.nf_SerialHC_SR04.Constants;
 namespace Driver.nf_Serial_HCSR04
 {
 
-	public class nf_Serial_HCSR04 
-	{
+    public class nf_Serial_HCSR04
+    {
 		private readonly SensorType pingByte;
 		static SerialDevice _serialDevice;
-		
+		byte[] data = new byte[4];
+		public event NativeEventHandler DataReceived;
+
 		// setup data writer for Serial Device output stream to ping device
 		DataWriter outputDataWriter = new DataWriter(_serialDevice.OutputStream);
 		// setup data read for Serial Device input stream to receive the distance
@@ -33,10 +39,26 @@ namespace Driver.nf_Serial_HCSR04
 			// get available ports
 			var serialPorts = SerialDevice.GetDeviceSelector();
 			// set GPIO functions for COM2 (this is UART1 on ESP32)
-			Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.COM2_TX);
-			Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.COM2_RX);
-			// open COM2
-			_serialDevice = SerialDevice.FromId("COM2");
+
+#if BUIID_FOR_ESP32
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            // COM2 in ESP32-WROVER-KIT mapped to free GPIO pins
+            // mind to NOT USE pins shared with other devices, like serial flash and PSRAM
+            // also it's MANDATORY to set pin funcion to the appropriate COM before instanciating it
+
+            // set GPIO functions for COM2 (this is UART1 on ESP32)
+            Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.COM2_TX);
+            Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.COM2_RX);
+
+            // open COM2
+            _serialDevice = SerialDevice.FromId("COM2");
+#else
+			///////////////////////////////////////////////////////////////////////////////////////////////////
+			// COM6 in STM32F769IDiscovery board (Tx, Rx pins exposed in Arduino header CN13: TX->D1, RX->D0)
+			// open COM6
+			_serialDevice = SerialDevice.FromId("COM6");
+#endif
+
 			// set parameters
 			_serialDevice.BaudRate = 9600;
 			_serialDevice.Parity = SerialParity.None;
@@ -57,12 +79,11 @@ namespace Driver.nf_Serial_HCSR04
 		public int getDistance()
         {
 			sensorPing(pingByte);
-			byte[] data = new byte[4];
 			// Attempt to read 4 bytes from the Serial Device input stream
 			// Format: 0XFF + H_DATA + L_DATA + SUM
-			var bytesRead = inputDataReader.Load(4);
+			var bytesRead = inputDataReader.Load(_serialDevice.BytesToRead);
 
-			if (bytesRead > 0)
+			if (bytesRead == 4)
 			{
 				inputDataReader.ReadBytes(data);
 
@@ -76,6 +97,46 @@ namespace Driver.nf_Serial_HCSR04
 				}
 			}
 				return 0;
+		}
+
+		/// <summary>
+		/// Serial data received event
+		/// </summary>
+		/// <param name="Sender"></param>
+		/// <param name="EventData"></param>
+		private void _Serial_DataReceived(object Sender, SerialDataReceivedEventArgs EventData)
+		{
+			int distanceValue;
+			int i;
+			// Attempt to read 4 bytes from the Serial Device input stream
+			// Format: 0XFF + H_DATA + L_DATA + SUM
+			var bytesRead = inputDataReader.Load(_serialDevice.BytesToRead);
+
+			if (bytesRead == 4)
+			{
+				inputDataReader.ReadBytes(data);
+
+				var distanceByte = (data[1] << 8) | data[2];
+				var sum = data[3];
+				var dataCheck = (data[0] + data[1] + data[2]) & 0x00ff;
+
+				if (dataCheck == sum)
+				{
+					distanceValue = (int)(data[1] + data[2]);
+					if (DataReceived != null)
+					{
+						DataReceived(0, 0, new DateTime());
+					}
+				}
+				else
+				{
+					for (i = 0; i < 3; ++i) // Need correct loop sequence
+					{
+						sensorPing(pingByte);
+					}
+					// Handle Error!
+				}
+			}
 		}
 	}
 }
