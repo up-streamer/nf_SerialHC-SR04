3c370c8d366202c805f1540d82c0766bcf54c01d
up_stream <up_stream@hotmail.com>
2021-06-28 16:03:09 -0300

    Organize test Program.

--- nf_SerialHC-SR04/Program.cs
+++ nf_SerialHC-SR04/Program.cs
@@ -11,10 +11,9 @@ namespace testnf_Serial_HCSR04
         private static Serial_HCSR04 sensor;
         public static void Main()
         {
-            sensor = new Serial_HCSR04(SensorType.AJ_SR04M, Mode.Serial_Auto);
-            sensor.ReadInterval = 1000;
-            
             // * To test receive data on demand, by pinging the device. *
+            //sensor = new Serial_HCSR04(SensorType.AJ_SR04M, Mode.Serial_LP_Bin);
+
             //for (int i = 0; i < 500; i++)
             //{
             //    int distance = sensor.GetDistance();
@@ -23,6 +22,9 @@ namespace testnf_Serial_HCSR04
             //}
 
             // * To test receive data when in auto mode, new data got from Timer thread.* 
+            sensor = new Serial_HCSR04(SensorType.AJ_SR04M, Mode.Serial_Auto);
+            sensor.ReadInterval = 1000;
+
             for (int i = 0; i < 500; i++)
             {
                 Debug.WriteLine($"distance = {sensor.Distance} mm " + $"Status. {sensor.status}" + $" --> count = {i}");
